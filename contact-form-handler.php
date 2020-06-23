1b2ad70cc7cb9b6a9cfa1edd99896034c4dbad6f
Danijel Doveden <danijeldoveden.55@gmail.com>
2020-06-23 18:07:42 +0200

    contact handler

---
+++ contact-form-handler.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Mail response page</title>
+    <style>
+    body {
+        color: #00C1FF;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100vw;
+        height: 100vh;
+        background-image: linear-gradient(60deg, rgba(236, 246, 248, 0.7) 15%, rgba(174, 188, 209, 0.7), rgba(233, 247, 250, 0.6) 85%);
+        background-size: cover;
+    }
+
+    div.m-auto {
+        width: 100%;
+        height: 40vh;
+    }
+
+    div.row {
+        padding: 10px;
+        background: #284866;
+        position: absolute;
+        margin: auto;
+        width: 100%;
+        bottom: 0;
+    }
+
+    a {
+        margin: auto;
+    }
+
+    img {
+        width: 100%;
+    }
+    </style>
+</head>
+
+<body>
+
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+try{
+    //Server settings
+    $mail->SMTPDebug = false;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'mail.npiflux.rs';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'office1@npiflux.rs';                     // SMTP username
+    $mail->Password   = '12345NPI12345NPI';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+        )
+        );
+    $from = $_POST["email"];
+    $name = $_POST["name"];
+    $body = $_POST["message"];
+    // $name = $_POST["name"];
+    $mail->addAddress('office1@npiflux.rs'); // Set the recipient of the message.
+    // $mail->From = $from;
+    // $mail->FromName = $name;
+    $mail->AddReplyTo($from, $name);
+    $mail->setFrom($from, $name); // Set the sender of the message.
+    $mail->Subject = 'Message from website'; // The subject of the message
+    $mail->Body = $body; // Set a plain text body.
+    if ($mail->send()) {?>
+        <div class="m-auto text-center">
+            <h2>
+                <?php 
+        echo 'Your message was sent successfully!' . "       $from"; ?>
+            </h2>
+            <div class="row no-gutters text-center m-auto">
+                <a href="#"><img src="img/npilogo.png" alt="logo footer"></a>
+                <p class="text-center w-100"><small>Copyright © RDdesign 2020. All Rights Reserved</small></p>
+            </div>
+        </div>
+        <?php
+        header( "refresh:3;url=https://npiflux.rs/" );
+    } 
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+} ;?>
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+        </script>
+    </body>
+    
+    </html>
--- js/main.js
+++ js/main.js
@@ -7,42 +7,82 @@ hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
-        // link.addEventListener)
     });
     lines.forEach(line => {
         line.classList.toggle("hamb-open");
     });
 });
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        lines.forEach(line => {
+            line.classList.remove("hamb-open");
+        });
+        navLinks.classList.remove("open");
+        links.forEach(link => {
+            link.classList.remove("fade");
+        });
+    });
+});
 
 $(() => {
-    if ($(window).width() <= 768) {
-        links.forEach(link => {
-            link.addEventListener("click", () => {
-                navLinks.classList.toggle("open");
-                links.forEach(link => {
-                    link.classList.toggle("fade");
-                });
-            });
-        })
-    }
 
     ////ANIMA
     function dodajAnimu() {
         var scrollPosition = $(window).scrollTop();
         console.log(scrollPosition);
-        if ($(window).width() <= 768) {
-            if (scrollPosition >= 2300) {
+        if ($(window).width() < 768) {
+            if (scrollPosition >= 1700) {
                 $(".who div.row").css("opacity", "1");
                 $(".who div.col-sm-12:nth-of-type(1)").addClass("who-anima-left");
                 $(".who div.col-sm-12:nth-of-type(2)").addClass("who-anima-right3");
             }
-            if (scrollPosition >= 2800) {
+            if (scrollPosition >= 2270) {
                 $(".why div.row").css("opacity", "1");
                 $(".why div.col-sm-12:nth-of-type(1)").addClass("who-anima-right2");
                 $(".why div.col-sm-12:nth-of-type(2)").addClass("who-anima-right");
             }
             if (scrollPosition >= 250) {
-                $("#services h3").addClass("anima-down-up")
+                $("#services h3").addClass("anima-down-up");
+            }
+        } else if ($(window).width() >= 768 && $(window).width() < 1024) {
+            if (scrollPosition >= 870) {
+                $("nav").addClass('navbg');
+            }
+            if (scrollPosition <= 870) {
+                $("nav").removeClass('navbg');
+            }
+            if (scrollPosition >= 1200) {
+                $(".who div.row").css("opacity", "1");
+                $(".who div.col-sm-12:nth-of-type(1)").addClass("who-anima-left");
+                $(".who div.col-sm-12:nth-of-type(2)").addClass("who-anima-right3");
+            }
+            if (scrollPosition >= 1700) {
+                $(".why div.row").css("opacity", "1");
+                $(".why div.col-sm-12:nth-of-type(1)").addClass("who-anima-right2");
+                $(".why div.col-sm-12:nth-of-type(2)").addClass("who-anima-right");
+            }
+            if (scrollPosition >= 250) {
+                $("#services h3").addClass("anima-down-up");
+            }
+        } else if ($(window).width() >= 1024 && $(window).width() < 1200) {
+            if (scrollPosition >= 870) {
+                $("nav").addClass('navbg');
+            }
+            if (scrollPosition <= 870) {
+                $("nav").removeClass('navbg');
+            }
+            if (scrollPosition >= 1700) {
+                $(".who div.row").css("opacity", "1");
+                $(".who div.col-sm-12:nth-of-type(1)").addClass("who-anima-left");
+                $(".who div.col-sm-12:nth-of-type(2)").addClass("who-anima-right3");
+            }
+            if (scrollPosition >= 2300) {
+                $(".why div.row").css("opacity", "1");
+                $(".why div.col-sm-12:nth-of-type(1)").addClass("who-anima-right2");
+                $(".why div.col-sm-12:nth-of-type(2)").addClass("who-anima-right");
+            }
+            if (scrollPosition >= 250) {
+                $("#services h3").addClass("anima-down-up");
             }
         } else {
             if (scrollPosition >= 2260) {
@@ -68,7 +108,7 @@ $(() => {
         }
     }
     $(window).on('scroll', () => {
-        dodajAnimu();;
+        dodajAnimu();
     });
     $("#services div.col-md-6").mouseover(function() {
         $("div.port-card", $(this)).addClass("service-anima");
@@ -79,5 +119,10 @@ $(() => {
         $("div.port-card").removeClass("service-anima");
         $(".card-inner, h4", $(this)).removeClass("card-anima");
     });
+
+    // setTimeout(() => {
+    //     $("div.title div.d-flex").addClass("arrow-anima2");
+    //     $("div.title div.d-flex").removeClass("arrow1-anima")
+    // }, 1600);
 
 });
